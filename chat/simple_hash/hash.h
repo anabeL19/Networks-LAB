99739345a79f7798136a46aad6d4927b5612f31a
anabeL19 <anabel.paredes@ucsp.edu.pe>
2019-04-14 18:59:54 -0500

    socket

--- chat/simple_hash/hash.h
+++ chat/simple_hash/hash.h
@@ -4,17 +4,6 @@ using namespace std;
 //simple hash
 string s_hash(string message)
 {
-//     int hash = 0;
-// 	for(unsigned int i=0; i<message.size(); i++){
-// 		hash += message[i];
-// 	}
-// // 
-// 	hash = hash%10;
-// 	string shash = to_string(hash);
-// 	for(unsigned int i=shash.size(); i<2; i++)
-// 		shash = '0'+shash;
-// // 
-// return shash;
     char buffer[4];
     int sum = 0;
     for(int i=0; i<message.size(); i++)
