13cf2088904a8858002a21332c0877f26e3d6bd9
anabeL19 <anabel.paredes@ucsp.edu.pe>
2019-04-14 20:11:51 -0500

    comparison between hash message

--- chat/simple_hash/hash.h
+++ chat/simple_hash/hash.h
@@ -1,12 +1,13 @@
 #include <string.h>
 
 using namespace std;
+
 //simple hash
 string s_hash(string message)
 {
     char buffer[4];
     int sum = 0;
-    for(int i=0; i<message.size(); i++)
+    for(unsigned int i=0; i<message.size(); i++)
     {
         sum += message[i];
     }
